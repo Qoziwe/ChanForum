ec0306126019de2f7f8da0d1f6c105d26983cad5
Qoziwe <leka.false@gmail.com>
2025-01-12 01:40:27 +0600

    Added requirements and deleted VENV

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\Desktop\newproject\venv
+set VIRTUAL_ENV=C:\Users\Cassian Comp\Desktop\New_folder\-ChanForum\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
