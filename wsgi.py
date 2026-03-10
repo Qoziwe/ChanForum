64fa9cc6728f40008c16251d60cd5c10ed51f0a9
Qoziwe <leka.false@gmail.com>
2026-03-10 17:42:15 +0500

    feat: Add WSGI configuration file.

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+# Добавляем путь к директории с app.py в sys.path, чтобы Python мог найти модуль app
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'ChanForum'))
+
+from app import app
+
+if __name__ == "__main__":
+    app.run()
